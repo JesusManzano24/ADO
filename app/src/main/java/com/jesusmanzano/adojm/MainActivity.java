1fbd216dd13d1930736d55325758e01a2b0cff07
JesusManzano24 <9991404825j@gmail.com>
2024-07-31 07:52:49 -0600

    first commit

---
+++ app/src/main/java/com/jesusmanzano/adojm/MainActivity.java
@@ -0,0 +1,73 @@
+package com.jesusmanzano.adojm;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+import com.bumptech.glide.Glide;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button inicio;
+    Button registro;
+    EditText user;
+    EditText password;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Inicialización de vistas
+        ImageView logoImageView = findViewById(R.id.Logo);
+        inicio = findViewById(R.id.Inicio);
+        registro = findViewById(R.id.Registro);
+        user = findViewById(R.id.user);
+        password = findViewById(R.id.password);
+
+        // Cargar y mostrar el GIF usando Glide
+        Glide.with(this)
+                .load(R.drawable.ado) // Cambia esto al recurso de tu GIF
+                .into(logoImageView);
+        // Listener para el botón de registro
+        registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+        // Listener para el botón de inicio
+        inicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Obtener las credenciales ingresadas
+                String usuarioIngresado = user.getText().toString();
+                String contraseñaIngresada = password.getText().toString();
+
+                // Verificar las credenciales
+                SharedPreferences prefs = getSharedPreferences("MisPreferencias", MODE_PRIVATE);
+                String usuarioGuardado = prefs.getString("usuario", "");
+                String contraseñaGuardada = prefs.getString("contraseña", "");
+
+                // Si las credenciales coinciden, iniciar nueva Activity
+                if(usuarioIngresado.equals(usuarioGuardado) && contraseñaIngresada.equals(contraseñaGuardada)) {
+                    Intent intent = new Intent(MainActivity.this, MainActivity3.class); // Reemplaza 'TerceraActivity' con el nombre de la clase de tu tercera pantalla
+                    startActivity(intent);
+                } else {
+                    // Mostrar mensaje de error
+                    Toast.makeText(MainActivity.this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
